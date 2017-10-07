cff9209f832e7d6870749f272e493ee4784641f1
Vnymark <vnymark@gmail.com>
2017-10-07 05:35:16 +0200

    Fixed functionality for creating and receiving messages.

--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', function(socket) {
    console.log('New user connected.');
 
-   socket.emit('newMessage', {
-      from: 'Bertil',
-      text: 'Testing.',
-      createdAt: 123123
-   });
-
    socket.on('createMessage', (message) => {
       console.log('createMessage', message);
+      io.emit('newMessage', {
+         from: message.from,
+         text: message.text,
+         createdAt: new Date().getTime()
+      })                                ;
    });
 
    socket.on('disconnect', () => {
